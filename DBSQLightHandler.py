2a05ca4dc36a7ff6c255ecdec925455d918962c8
noanelersa <noa.nelersa@gmail.com>
2024-12-15 22:25:22 +0200

    implemented the abstracted methods in the sqlight handler. not done

--- DBSQLightHandler.py
+++ DBSQLightHandler.py
@@ -8,5 +8,55 @@ class SQLightDBHandler (DBHandler):
         self.connection= sqlite3.connect(name)
         self.cursor = self.connection.cursor()
 
-   # def createTable(self):
-       # self.cursor.execute().....
+    def createTable(self):
+        try:
+            self.cursor.execute(
+                """CREATE TABLE IF NOT EXISTS Asteroids (
+                    name TEXT PRIMARY KEY,
+                    diameter REAL NOT NULL,
+                    approach_date TEXT NOT NULL,
+                    velocity REAL NOT NULL )""")     
+        #except sqlite3.   ???????
+
+        finally:
+            self.connection.commit()
+            self.cursor.close()
+    
+    def insertData(self, data):
+        try:
+            self.cursor.execute(
+            """INSERT INTO Asteroids(name, diameter, approach_date,velocity) 
+            VALUES (?, ?, ?, ?)""",
+            data['name'],data['diameter'], data['approach_data'],data['velocity']
+        )
+        #except sqlite3.  ??????????
+        finally:
+            self.connection.commit()
+            self.cursor.close()
+
+    
+    def readData(self, query, params=()):
+        self.cursor.execute(query, params)
+        result = self.cursor.fetchall()
+        self.connection.commit()
+        self.cursor.close()
+        
+    
+    def updateData(self, query, params):
+        self.cursor.execute(query,params)
+        self.connection.commit()
+        self.cursor.close()
+
+    def deleteData(self, query, params):
+        self.cursor.execute(query,params)
+        self.connection.commit()
+        self.cursor.close()
+
+    def close_connection (self):
+        self.connection.close()
+    
+
+
+ 
+
+       
--- api_nasa_client.py
+++ api_nasa_client.py
@@ -11,6 +11,7 @@ class APInasaClient(APIClient):
         url= f"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={self.api_key}"
         data = super().get(url)
         #print (data)
+        #opening the json to a file to configure its sturcture for the transformer function
         with open("nasa_data.json", "w") as file:
             json.dump(data, file, indent=4)
         return data
