eaef9ae90ba064c051735638cfa2b44e3e54b901
noanelersa <noa.nelersa@gmail.com>
2024-12-15 04:31:56 +0200

    wrote the transform function, didnt check for exeptions yet

--- api_nasa_client.py
+++ api_nasa_client.py
@@ -1,5 +1,6 @@
 import requests
 from api_client import APIClient
+import json
 
 
 class APInasaClient(APIClient):
@@ -8,9 +9,11 @@ class APInasaClient(APIClient):
 
     def fetch_data(self, start_date, end_date):
         url= f"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={self.api_key}"
-        result = super().get(url)
-        #print (result)
-        return result
+        data = super().get(url)
+        #print (data)
+        with open("nasa_data.json", "w") as file:
+            json.dump(data, file, indent=4)
+        return data
 
 if __name__ == "__main__":
     api_key = "atGTDOlBIj4CkCcWdEpTTJfQ3okIe5kBnkgRpB0N" 
